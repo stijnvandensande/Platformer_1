fecf49cf03768341a785b59aac8b9c1f65812126
stijnvandensande <stijn.vandensande@student.uhasselt.be>
2026-01-05 23:45:49 +0100

    Levle 3 afgemaakt lvl 4 is trash ma is work in progress nog

--- src/main/java/be/inf1/platformer1/Level.java
+++ src/main/java/be/inf1/platformer1/Level.java
@@ -52,8 +52,8 @@ public class Level {
             
         
             //Glass
-            blocks.add(new BlockGlass(100, boardHeight-100, 100, 20));      //overhang
-            blocks.add(new BlockGlass(100, boardHeight-170, 100, 20));      //brug
+            blocks.add(new BlockGlass(100, boardHeight-100, 100, 20));      //brug
+            blocks.add(new BlockGlass(100, boardHeight-170, 100, 20));      //overhang
             
             //JumpPad
             blocks.add(new BlockJumpPad(800,boardHeight-550,100,20));
@@ -74,7 +74,7 @@ public class Level {
            
             
             blocks.add(new BlockPlatform(900, boardHeight-300, 80, 300));   // rechter muur onder
-            blocks.add(new BlockGlass(900, 480, 20, 700-480));                  //glassmuur
+            blocks.add(new BlockGlass(900, 480, 20, 700-480));              //glassmuur
             blocks.add(new BlockPlatform(900, 0, 80, 480));                 // rechter muur boven
 
             
@@ -97,26 +97,82 @@ public class Level {
             this.respawnCoordY = boardHeight-30;
             
             blocks.add(new BlockPlatform(1050+500-62, boardHeight-20, 125, 20));//beginplatform
+            blocks.add(new BlockPlatform(1050+500-62-20, boardHeight-30, 20, 20));//stopper voor mocht je nog q aan het indrukken zijn zodat je niet onmiddelijk dood gaat.... ja is ook een skill issue
             
             blocks.add(new BlockLava(0, boardHeight-10, boardWidth, 10));       //ground lava
             blocks.add(new BlockLava(boardWidth-20, 0, 20, boardHeight));       //side lava            
-                        
+            blocks.add(new BlockIce(0, 0, 10, boardHeight));                    // linker wall anticheat
+            
+            //jump1
             blocks.add(new BlockPlatform(250, boardHeight-135, 500, 20));       //Slimeplatform
             blocks.add(new BlockSlime(250, boardHeight-150, 500, 15));          //SLIME
-            blocks.add(new BlockLava(400, boardHeight-500, 20, 300));       //anti cheese lava            
-            blocks.add(new BlockPlatform(100, boardHeight-500, 80, 300));   // linker muur
-            blocks.add(new ExitBlock(120, boardHeight-600, 60, 40));              //temp test Exit
-            blocks.add(new BlockPlatform(boardWidth-200, 60, 200, 20));         //Exit platform
-            blocks.add(new ExitBlock(boardWidth-120, 20, 60, 40));              //Exit
+            blocks.add(new BlockLava(400, boardHeight-500, 20, 315+10));        //anti cheese lava
+            blocks.add(new BlockPlatform(100, boardHeight-500, 80, 300));       // linker muur
+            
+            //jump2
+            blocks.add(new BlockLava(0, boardHeight-600, 260, 40));             // Roof lava jump 1
+            blocks.add(new BlockPlatform(100, 0, 80, boardHeight-600));         // linker muur boven
+            blocks.add(new BlockLava(400, boardHeight-500, 20, 315));           //anti cheese lava
+            
+            //jump3
+            blocks.add(new BlockPlatform(750, boardHeight-135-250, 500, 20));   //Slimeplatform in het midden
+            blocks.add(new BlockSlime(750, boardHeight-400, 500, 15));          //SLIME midden groot
+            blocks.add(new BlockLava(400, boardHeight-500-315, 20, 315));       //anti cheese lava
+            
+            blocks.add(new BlockSlime(960, boardHeight-470, 80, 15));           //SLIME klein midden boven
+            blocks.add(new BlockLava(990, 0, 20, boardHeight-470));             //anti cheese lava
+            blocks.add(new BlockLava(990, boardHeight-400-20, 20, 20));         //VIERKANTE Hindernis tussen slimes
+            
+            blocks.add(new BlockPlatform(boardWidth-200, 160+150, 180, 20));         //Exit platform
+            blocks.add(new ExitBlock(boardWidth-120, 120+150, 40, 40));              //Exit
 
 
         
         }
         
         
+        if (levelNumber == 4) { //still work in progress
+
+            this.respawnCoordX = boardWidth-120;
+            this.respawnCoordY = 120;
+            
+            blocks.add(new BlockPlatform(boardWidth-183, 145, 125, 20));        //beginplatform
+
+            blocks.add(new BlockIce(0, 0, 10, boardHeight));                    // linker anticheat
+            blocks.add(new BlockIce(boardWidth-10, 0, 10, boardHeight));        // rechter anticheat
+            
+            blocks.add(new BlockSlime(450,boardHeight-20,boardWidth-450,20));   //Onder Slime
+            
+            blocks.add(new BlockLava(450, 0, 80, 700));                         // kolom 1
+            blocks.add(new BlockLava(750, 220, 80, 650));                       // kolom 2
+            blocks.add(new BlockLava(1050, 0, 80, 700));                        // kolom 3
+            blocks.add(new BlockLava(1350, 260, 80, 600));                      // kolom 4
+
+            blocks.add(new BlockJumpPad(boardWidth-80, boardHeight-20, 80, 20));//recover als nodig
+            
+            
+            
+            /*
+            blocks.add(new BlockLava(100, boardHeight-120, 500, 120));
+
+            blocks.add(new BlockLava(420, boardHeight-180, 80, 60));
+            blocks.add(new BlockLava(160, boardHeight-380, 300, 40));
+
+
+            blocks.add(new BlockJumpPad(820, boardHeight-220, 120, 30));
+
+            blocks.add(new BlockJumpPad(1300, boardHeight-280, 120, 30));
+
+            blocks.add(new BlockPlatform(200, boardHeight-220, 280, 40));
+            blocks.add(new ExitBlock(260, boardHeight-260, 55, 40));
+        */
+        }
         
         
-        if (levelNumber == 4) {
+        
+        
+        
+        if (levelNumber == 5) {
             this.respawnCoordX = 50;
             this.respawnCoordY = boardHeight-110;
             
